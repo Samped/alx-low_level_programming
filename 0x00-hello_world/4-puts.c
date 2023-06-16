4f7ecbb992b041f02333faed6a0d16683b377555
Samped <michaelsamuelpedro@gmail.com>
2023-06-15 23:28:05 -0700

    put program C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 0 (success)
  */
-init main(void)
+int main(void)
 {
-	put("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
