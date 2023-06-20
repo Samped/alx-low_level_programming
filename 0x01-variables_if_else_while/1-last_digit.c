66a7cda9a92c3a950b6408a29d0fad424aaf2794
Samped <michaelsamuelpedro@gmail.com>
2023-06-19 19:21:20 -0700

    if statement in c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
+/**
+ *  more headers goes there
+ *
+ * betty style doc for function main goes there 
+ */
+
 int main(void)
+
 {
         int n;
 
@@ -17,5 +23,5 @@ int main(void)
         else
                 printf("Last digit of %d is %d and is less than 6 and not 0\n", n , n % 10);
 
-        return 0;
+        return (0);
 }
