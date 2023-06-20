4b93132c47e65c915319db4526d1aaeb7d927b51
Samped <michaelsamuelpedro@gmail.com>
2023-06-20 13:58:28 -0700

    printing of alphabet in lowercase by importing self header and using _putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *print_alphabet in lowercase
- * follwed by new line
- * Return: nothing
+ *print_alphabet in lowercase - Check Description
+ * Description: it prints alphabet follwed by new line
+ * Return: Nothing
  */
 
 void print_alphabet(void)
 {
 	char i;
 
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
 }
