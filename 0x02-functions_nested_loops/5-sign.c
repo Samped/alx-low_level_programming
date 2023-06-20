3c7c3bc8aa7eccda4cb57747fcdf95e59edb268a
Samped <michaelsamuelpedro@gmail.com>
2023-06-20 16:29:10 -0700

    using if statement with putchar

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+int print_sign(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		i = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		i = 0;
+		_putchar('0');
+	}
+	else
+	{
+		i = -1;
+		_putchar('-');
+	}
+
+	return (i);
+}
