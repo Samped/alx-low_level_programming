0e34052c455ec1f871b782c9b5a0206fb8f7d162
Samped <michaelsamuelpedro@gmail.com>
2023-06-20 15:09:02 -0700

    printing alphabet using self header 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,14 @@
  */
 
 void print_alphabet_x10(void)
-
 {
 	char i;
-        int j;
+	int j;
 
 	for (j = 0; j < 10; j++)
-
 	{
 		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
-
 		_putchar('\n');
 	}
-}	
+}
