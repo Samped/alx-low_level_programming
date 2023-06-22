21291047ed042321d07b73e91737c68daaef7bbb
Samped <michaelsamuelpedro@gmail.com>
2023-06-22 13:07:23 -0700

    checking if an input is a uppercase or lowercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
-/*
- * _isupper - check description
- * @c: is an input of alphabet
- * description: when the input alphabet is an uppercase it returns 1 else it will return  0
+/**
+ * _isupper - A function that checks for uppercase character.
+ * @c: An input character
+ * Return: 1 if c is uppercase or 0 otherwise
  */
-
 int _isupper(int c)
 {
 	char uppercase;
-	int isupper= 0;
+	int isupper = 0;
 
-	for(uppercase = 'A'; uppercase<= 'Z'; uppercase++)
+	for ( uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 	{
 		if (c == uppercase)
 		{
 			isupper = 1;
 			break;
 		}
-
 	}
+
 	return (isupper);
 }
