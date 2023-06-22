7c7b97b050231fcdb681058736b84a0575b59bee
Samped <michaelsamuelpedro@gmail.com>
2023-06-22 13:26:11 -0700

    checks if the input is a number

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * _isdigit - check description
+ * @c:input of numbers
+ * description: checks whether the input is number or not
+ * Return 1 if it's a number else 0
+ */
+
+int _isdigit(int c)
+{
+	int digit;
+	int isdigit = 0;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		if (c == digit)
+		{
+			isdigit = 1;
+			break;
+		}
+	}
+	return (isdigit);
+}	
