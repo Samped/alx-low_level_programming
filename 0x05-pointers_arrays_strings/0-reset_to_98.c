a3ec8d8f88426f920bf2ffd854f31b15e2ef7be3
Samped <michaelsamuelpedro@gmail.com>
2023-06-26 17:25:38 -0700

    updating a value of an int function using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * void rest_to_98: check description
+ * int *n: pointer 
+ * Describtion: A function that it's pointer updates the value of n
+ * Return: Nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
