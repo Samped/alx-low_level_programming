ec5859685430b18e047102c066dedc503ccc1b3d
Samped <michaelsamuelpedro@gmail.com>
2023-06-26 17:47:10 -0700

    swapping of two values of pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * void swap_int - A function that swaps the values of the two
+ * swap_int - A function that swaps the values of the two
  * pointers that is a and b
  * @a: a pointer that stores the address of an interger
- * @b: a pointer that stores the adrres of an interger 
+ * @b: a pointer that stores the addrres of an interger 
  * Return: Nothing
  */
 
